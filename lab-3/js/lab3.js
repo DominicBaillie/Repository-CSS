9965a8aa0062cd22e992c6f6993a0d14af59bf46
dominicbaillie <dominicbaillie8@gmail.com>
2026-03-18 12:36:39 -0400

    Finished Lab 3

--- lab-3/js/lab3.js
+++ lab-3/js/lab3.js
@@ -1,19 +1,25 @@
-// STEP 6: Nab all the SUMMARY elements
-const tabs = document.querySelectorAll("tabs");
+document.addEventListener("DOMContentLoaded", () => {
+	const tabs = document.querySelectorAll('[role="tab"]');
+	const panels = document.querySelectorAll('[role="tabpanel"]');
 
-tabs.forEach((tab) => {
-tab.addEventListener("click", (event) => {
-		if (event.target.open)
-		{
-			tab.forEach((detail) => 
-			{
-				if(tab !== event.target)
-				{
-					tab.removeAttribute("open");
-				}
+	tabs[0].classList.add("active");
+	panels[0].classList.add("active");
 
-			});
-		}
+	tabs.forEach(tab => {
+		const link = tab.querySelector("a");
+
+		link.addEventListener("click", e => {
+			
+
+			tabs.forEach(t => t.classList.remove("active"));
+			panels.forEach(p => p.classList.remove("active"));
+
+			tab.classList.add("active");
+
+			const target = document.querySelector(link.getAttribute("href"));
+			target.classList.add("active");
+
+			e.preventDefault();
+		});
 	});
-
 });
